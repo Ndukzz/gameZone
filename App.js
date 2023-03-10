93f25994aafee714c2f90ebfb901a768983706e1
ndukz <ebukaemannuel@gmail.com>
2023-03-10 22:51:59 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Font from 'expo-font'
+import Home from './screens/Home';
+import { useState } from 'react';
+import Apploading from 'expo-app-loading'
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+})
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ fontsLoaded, setFontsLoaded ] = useState(false)
+
+  if(fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <Apploading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//IMPLEMENT THE EXPO-SPLASH-SCREEN INTO THIS ASAP
---
+++ screens/Review.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Review = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Review Screen</Text>
+    </View>
+  )
+}
+
+export default Review
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+})
